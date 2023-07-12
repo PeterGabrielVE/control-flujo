6af612a32ea740af8b6804b06b89a57b88042767
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-12 16:24:24 -0400

    if, elif, else

---
+++ 02-if.py
@@ -0,0 +1,11 @@
+edad=60
+
+if edad > 54:
+    print("Puede ver la pelicula con descuento")
+elif edad > 17:
+    print("Puede ver la pelicula")
+else:
+    print("No puedes entrar")
+    print("ve a otro lado")
+
+print("Listo")
