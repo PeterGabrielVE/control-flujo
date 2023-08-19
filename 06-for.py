5a3fd7ec4b2724f25584fb393f581396ad6b360f
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-19 16:26:55 -0400

    for else

--- 06-for.py
+++ 06-for.py
@@ -1,2 +1,8 @@
+buscar = 8
 for numero in range(5):
     print(numero)
+    if numero == buscar:
+        print("encontrado", buscar)
+        break
+else:
+    print('No encontre el numero entregado')
